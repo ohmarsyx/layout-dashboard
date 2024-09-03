85a4dd28266779448af3a6d0af28492cc9f7501f
ohmarsyx <ohmarsy@odds.team>
2024-09-03 22:38:53 +0700

    fix responsive bug

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const handleHamburger = () => {
   const hamburger = document.querySelector(".hamburger-icon");
   const sidebar = document.querySelector(".sidebar");
   const main = document.querySelector(".main-container");
+
   hamburger.classList.toggle("active");
 
   if (window.innerWidth <= 550) {
@@ -23,12 +24,27 @@ const handleXIcon = () => {
   const main = document.querySelector(".main-container");
 
   xIcon.classList.toggle("active");
+
   if (sidebar.style.display === "flex" || sidebar.style.display === "") {
     sidebar.style.display = "none";
     main.style.display = "flex";
   } else {
     sidebar.style.display = "flex";
-    main.stle.display = "none"
+    main.style.display = "none";
+  }
+};
+
+const checkResponsive = () => {
+  const sidebar = document.querySelector(".sidebar");
+  const main = document.querySelector(".main-container");
+
+  if (window.matchMedia("(max-width: 550px)").matches) {
+    sidebar.style.width = "100vw";
+    sidebar.style.backgroundColor = "white";
+  } else {
+    sidebar.style.width = "340px"; 
+    sidebar.style.backgroundColor = ""; 
+    main.style.display = "flex"; 
   }
 };
 
@@ -36,3 +52,6 @@ document
   .querySelector(".hamburger-icon")
   .addEventListener("click", handleHamburger);
 document.querySelector(".x-icon").addEventListener("click", handleXIcon);
+
+window.addEventListener("DOMContentLoaded", checkResponsive);
+window.addEventListener("resize", checkResponsive);
